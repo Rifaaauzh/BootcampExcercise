2915d4f0e01e74a9a7c7ae876e73bf4aed10860f
Rifa <rifafau1155@gmail.com>
2026-03-31 14:04:58 +0700

    initial commit

---
+++ Task1_bootcamp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string input = Console.ReadLine();
+int userInput = int.Parse(input);
+
+for (int i = 1; i <= userInput; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("foo");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("bar");
+    }
+    if (i % 3 == 0 && i % 5 == 0 )
+    {
+        Console.WriteLine("foobar");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
