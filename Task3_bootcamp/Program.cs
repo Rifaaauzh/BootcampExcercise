3c01e18723f91bfe6f78e52d8ad327e6c3fa383a
Rifa <rifafau1155@gmail.com>
2026-04-13 11:46:29 +0700

    Task 3 added

---
+++ Task3_bootcamp/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = Console.ReadLine();
+int userInput = int.Parse(input);
+
+List<string> result = new List<string>();
+
+for (int x = 1; x <= userInput; x++)
+{
+    string output = "";
+
+    if (x % 3 == 0)
+        output += "foo";
+    if (x % 4 == 0)
+        output += "baz";
+    if (x % 5 == 0)
+        output += "bar";
+    if (x % 7 == 0)
+        output += "jazz";
+    if (x % 9 == 0)
+        output += "huzz";
+    if (output == "")
+        output = x.ToString();
+
+    result.Add(output);
+}
+
+Console.WriteLine(string.Join(", ", result));
